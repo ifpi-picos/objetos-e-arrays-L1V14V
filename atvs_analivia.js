d02798f12a702459e2940c12f19c54ab2ecac7e0
liv.farias00 <131826367+L1V14V@users.noreply.github.com>
2024-09-13 10:07:00 -0300

    Update atvs_analivia.js

--- atvs_analivia.js
+++ atvs_analivia.js
@@ -3,15 +3,15 @@ const prompt = require('prompt-sync')();
 adicionar os elementos 40 e 50 ao final do array. Exiba o array
 resultante.*/
 
-/*const numeros = [10, 20, 30]
+const numeros = [10, 20, 30]
 
 numeros.push(40,50)
 
 console.log(numeros)
 
-6) Crie um array com os elementos [5, 10, 15, 20]. Use o método pop() para
+/*6) Crie um array com os elementos [5, 10, 15, 20]. Use o método pop() para
 remover o último elemento do array. Exiba o array resultante e o
-elemento removido.
+elemento removido.*/
 
 const num = [5, 10, 15 , 20]
 let ultimoNumero = num[3]
@@ -22,18 +22,18 @@ num.pop(3)
 console.log(num)
 console.log("o elemento removido é: " + ultimoNumero ).
 
-7) Dado o array let numeros = [1, 2, 3, 4, 5], use o método shift() para
+/*7) Dado o array let numeros = [1, 2, 3, 4, 5], use o método shift() para
 remover o primeiro elemento do array. Exiba o array resultante e o
-elemento removido
+elemento removido.*/
 
 const numeros = [1,2,3,4,5]
 
 let remove_elemento = numeros.shift()
 console.log(numeros , " o elemento retirado é:", remove_elemento)
 
-8) Crie um array com os elementos ['vermelho', 'verde']. Use o método
+/*8) Crie um array com os elementos ['vermelho', 'verde']. Use o método
 unshift() para adicionar os elementos 'azul' e 'amarelo' no início do array.
-Exiba o array resultante
+Exiba o array resultante.*/
 
 const cores = ['vermelho', 'verde']
 
@@ -42,9 +42,9 @@ let adiciona = cores.unshift('azul', 'amarelo')
 console.log(cores)
 
 
-9) Dado o array let frutas = ['maçã', 'banana'] e o array let maisFrutas =
+/*9) Dado o array let frutas = ['maçã', 'banana'] e o array let maisFrutas =
 ['laranja', 'pera'], use o método concat() para combinar os dois arrays em
-um novo array e exiba o resultado
+um novo array e exiba o resultado*/
 
 let frutas = ['maça', 'banana']
 let maisFrutas = ['laranja', 'pera']
@@ -53,9 +53,9 @@ let juntando = frutas.concat(maisFrutas)
 
 console.log(juntando)
 
-10) Dado o array let animais = ['cachorro', 'gato', 'pássaro', 'peixe'], use o
+/*10) Dado o array let animais = ['cachorro', 'gato', 'pássaro', 'peixe'], use o
 método slice() para criar um novo array contendo os elementos do índice
-1 ao índice 3 (não inclusivo). Exiba o novo array.)
+1 ao índice 3 (não inclusivo). Exiba o novo array.)*/
 
 
 
@@ -64,9 +64,9 @@ let animais = ['cachorro', ' gato', ' pássaro','peixe']
 let maisAnimais = animais.slice(1,3)
 console.log(maisAnimais)..
 
-11) Dado o array let cidades = ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte',
+/*11) Dado o array let cidades = ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte',
 'Salvador', 'Fortaleza'], use o método splice() para remover as cidades
-Belo Horizonte e Salvador do array.
+Belo Horizonte e Salvador do array.*/
 
 let cidade = ['São Paulo','Rio de Janeiro','Belo Horizonte','Salvador','Fortaleza']
 
@@ -74,9 +74,9 @@ let remova_cidades = cidade.splice(2,2)
 
 console.log(cidade)
 
-12) Dado o array let numeros = [50, 10, 1, 30, 20, 2], use o método sort()
+/*12) Dado o array let numeros = [50, 10, 1, 30, 20, 2], use o método sort()
 para ordenar os elementos em ordem crescente. Exiba o array
-resultante.
+resultante.*/
 
 
 let numeros = [50,10,1,30,20,2]
@@ -85,7 +85,7 @@ numeros.sort((a, b) => a - b)
 console.log(numeros)
 
 
-13) Escreva um programa que solicita ao usuário o nome de 5 filmes e
+/*13) Escreva um programa que solicita ao usuário o nome de 5 filmes e
 armazene em um array. Em seguida, exiba no console os filmes
 informados*/
 
